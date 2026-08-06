764bdb6acb3021f338ac9b9842b760b37a352f93
Lujala Dangol <lujaladangol0@example.com>
2026-08-06 13:26:02 +0545

    feat: configure PostgreSQL connection

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,21 @@
 import "dotenv/config";
 import app from "./app.js";
+import pool from "./config/database.js";
 
 const PORT = Number(process.env.PORT) || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+const startServer = async () : Promise<void> => {
+  try{
+    await pool.query("SELECT NOW()"); // Test database connection
+    console.log("PostgreSQL connected successfully");
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  }catch (error) {
+    console.error("Error connecting to PostgreSQL:", error);
+    process.exit(1); // Exit the process with an error code 
+  }
+};
+
+startServer();
